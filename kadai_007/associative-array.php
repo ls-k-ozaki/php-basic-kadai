3b6cd276fc8150050c72ee592f00e0f0c4abd4da
KazutoOzaki <95326109+ls-k-ozaki@users.noreply.github.com>
2023-12-20 21:23:43 +0900

    Add files via upload
    kadai_007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+  <p>
+    <?php 
+      $array = [
+        'name' => 'onion',
+        'price' => '200',
+        'weight' => '160'
+      ];
+        
+        print_r ($array);
+    ?>
+  </p>
+  
+</body>
+</html>
